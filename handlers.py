f3cc069765c19b38ddef019d61709faf6e5177e6
bryantc99 <bachen@stanford.edu>
2017-04-26 07:14:20 -0700

    f

--- handlers.py
+++ handlers.py
@@ -84,7 +84,7 @@ class WelcomeHandler(BaseHandler):
         #if(city != "Paris"):
          #   self.render("about.html", title="Oxford Experiments", oid = oid)
         #else:
-        print "oid: " + self.get_argument(oid)
+        print "oid: " + self.get_argument("oid")
         self.render("welcome.html", title="Oxford Experiments", oid = oid)
     def post(self, oid):
         #oid = self.get_argument('oid')
