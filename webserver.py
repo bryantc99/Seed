5e7ab61428f5e382a6498afd7be01a9af6580fa4
bryantc99 <bachen@stanford.edu>
2017-05-10 08:33:19 -0700

    fix

--- webserver.py
+++ webserver.py
@@ -537,7 +537,7 @@ class GameConnection(SockJSConnection):
                 game_id = msg['game_id']
                 self.broadcast(GameConnection.PARTICIPANTS[game_id], message)
             elif msg_type == GameConnection.QUIT_MSG:
-                logger.info("[GameConnection] Player " + msg['subject_no'] + " disconnected from game " + msg['game_id'])
+                logger.info("[GameConnection] Player " + str(msg['subject_no']) + " disconnected from game " + msg['game_id'])
                 WaitingRoomConnection.DROPPED.append(msg['subject_no'])
                 WaitingRoomConnection.TOT_PLAYERS = WaitingRoomConnection.TOT_PLAYERS - 1
                 logger.info("Tot players " + WaitingRoomConnection.TOT_PLAYERS)
