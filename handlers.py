087a42ea8c769c914667478404bf39a1b227b937
bryantc99 <bachen@stanford.edu>
2017-05-10 10:03:14 -0700

    asdf

--- handlers.py
+++ handlers.py
@@ -57,7 +57,8 @@ class RegisterHandler(BaseHandler):
             })
         print "About: " + name
 
-        self.render("about.html", title="Oxford Experiments", oid = result.inserted_id)
+        self.render("about.html", title="Oxford Experiments", oid = 1)
+        #self.render("about.html", title="Oxford Experiments", oid = result.inserted_id)
         #self.render("welcome.html", title="Oxford Experiments", oid = result.inserted_id, round = 1)
 
 class QuizHandler(BaseHandler):
