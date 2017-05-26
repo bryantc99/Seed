bd10af347d72dca4850fc8d2d72c2b5076d26242
bryantc99 <bachen@stanford.edu>
2017-05-25 22:25:56 -0700

    asdf

--- webserver.py
+++ webserver.py
@@ -772,7 +772,7 @@ def createSession(sessionType, num):
     print "Creating session of type " + sessionType + " with " + num + " players."
     if(SessionConnection.present_subjects and len(SessionConnection.present_subjects) >= 0):
         print str(num) + " subjects: " + str(len(SessionConnection.present_subjects)) + " " + str(SessionConnection.present_subjects)
-        SessionConnection.admin_client.broadcast(random.sample(SessionConnection.present_subjects, num), json.dumps({'type': SessionConnection.ACTIVATE_MSG}))
+        SessionConnection.admin_client.broadcast(random.sample(SessionConnection.present_subjects, 1), json.dumps({'type': SessionConnection.ACTIVATE_MSG}))
 
 
 def main():
