36cf7e31c63bd0e4b7c2e3bbf44992b3e262bd7a
bryantc99 <bachen@stanford.edu>
2017-06-01 00:10:40 -0700

    f

--- static/js/admin.js
+++ static/js/admin.js
@@ -108,7 +108,8 @@ function connect() {
 
 
                             // Append the text to <li>
-        $('#sessionList').append("<li>asdf</li>"); 
+        $('#sessionList').append("<li>asdf</li>");
+        $('#sessionList').css("display", "none"); 
         console.log("new list");
     }
 
