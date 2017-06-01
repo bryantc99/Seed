f9fb439177649ee1b1169c9894c057717b81fea9
bryantc99 <bachen@stanford.edu>
2017-05-31 23:03:25 -0700

    asd

--- webserver.py
+++ webserver.py
@@ -774,9 +774,10 @@ def createSession(sessionType, num):
     if(SessionConnection.present_subjects and len(SessionConnection.present_subjects) >= 0):
         print str(num) + " subjects: " + str(len(SessionConnection.present_subjects)) + " " + str(SessionConnection.present_subjects)
         sample = random.sample(SessionConnection.present_subjects, int(num))
+        sample.append(SessionConnection.admin_client)
         SessionConnection.admin_client.broadcast(sample, json.dumps({'type': SessionConnection.ACTIVATE_MSG}))
-        SessionConnection.admin_client.broadcast(SessionConnection.admin_client, json.dumps({'type': SessionConnection.ACTIVATE_MSG}))
-        
+        #SessionConnection.admin_client.broadcast(SessionConnection.admin_client, json.dumps({'type': SessionConnection.ACTIVATE_MSG}))
+
 
 
 
